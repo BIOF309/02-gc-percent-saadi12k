7dad4143989d7d83674d46f260a7a65067cf87a4
saadi12k <hassan.saadi@nih.gov>
2017-10-23 19:51:28 -0400

    Correct percent calculation

--- GC_calculator.py
+++ GC_calculator.py
@@ -13,5 +13,5 @@ GC=G+C
 print("G's =" +str(G) + " , C's =" +str(C) + " , and G's+C's =" +str(GC) + ".")
 ATCG= GC+flu_ns1_seq.count('A')+flu_ns1_seq.count('T')
 print("The total number of nucletides in the sequence is " + str(ATCG))
-GC_percent=GC/ATCG
+GC_percent=(GC/ATCG)*100
 print("The GC percent of the sequence is "+str(GC_percent))
